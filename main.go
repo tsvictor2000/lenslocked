e3f2d069c95d90b7ff0133bdb975e74223f1adbc
Viktor <tsvictor@mail.ru>
2024-02-26 20:00:10 +0600

    Second without comment

--- main.go
+++ main.go
@@ -35,46 +35,12 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	`)
 }
 
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	// fmt.Fprintln(w, r.URL.Path)
-// 	// fmt.Fprintln(w, r.URL.RawPath)
-
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		pageNotFoundHandler(w, r)
-// 	}
-// }
-
-// type Router struct{}
-
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	case "/faq":
-// 		faqHandler(w, r)
-// 	default:
-// 		pageNotFoundHandler(w, r)
-// 	}
-// }
-
 func main() {
-	// http.HandleFunc("/", homeHandler)
-	// http.HandleFunc("/contact", contactHandler)
-	// var router Router
-	// router = pathHandler
 	r := chi.NewRouter()
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
 	r.NotFound(pageNotFoundHandler)
 	fmt.Println("Stearting the server on :3000...")
-	// http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
 	http.ListenAndServe(":3000", r)
 }
