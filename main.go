2d31b1fd19c9ce6bb8cedd2aebc3a87d71468541
Viktor <tsvictor@mail.ru>
2024-02-26 23:01:48 +0600

    Очистка списка от нумерации

--- main.go
+++ main.go
@@ -29,16 +29,16 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprint(w, `<h1>FAQ page</h1>
 	<ul>
-		<li><b>1 Куда бы вы хотели поехать отдыхать летом?</b> В Турцию</li>
-		<li><b>2 Сколько вас человек?</b> Нас 4 человека</li>
-		<li><b>3 Сколько будет стоит путевка?</b> Путевка на 4-х стоит 2 млн. тенге</li>
+		<li><b>Куда бы вы хотели поехать отдыхать летом?</b> В Турцию</li>
+		<li><b>Сколько вас человек?</b> Нас 4 человека</li>
+		<li><b>Сколько будет стоит путевка?</b> Путевка на 4-х стоит 2 млн. тенге</li>
 	</ul>
 	`)
 }
 
 func main() {
 	r := chi.NewRouter()
-	
+
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
