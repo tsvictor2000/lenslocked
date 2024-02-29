bf076f6f1c48f570605c0617cd5974c8f71f4872
Viktor <tsvictor@mail.ru>
2024-02-29 18:01:21 +0600

    Распределено по пакетам

--- main.go
+++ main.go
@@ -4,39 +4,11 @@ import (
 	"fmt"
 	"lenslocked/controllers"
 	"lenslocked/views"
-	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
-
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	t, err := views.Parse(filepath)
-	if err != nil {
-		log.Printf("Ошибка парсинга %v", err)
-		http.Error(w, "Произошла ошибка парсинга шаблона", http.StatusInternalServerError)
-		return
-	}
-
-	t.Execute(w, nil)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tplpath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tplpath)
-}
-
-func pageNotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>Page not found</h1>")
-
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
-}
 
 func main() {
 	// парсинг шаблонов
@@ -48,9 +20,21 @@ func main() {
 	}
 	r.Get("/", controllers.StaticHandler(tpl))
 
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
-	r.NotFound(pageNotFoundHandler)
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 
 	fmt.Println("Stearting the server on :3000...")
 
