3c86f82768f9d5d96ab669c91953cde6420d476d
Viktor <tsvictor@mail.ru>
2024-07-31 17:41:47 +0500

    hash

---
+++ cmd/bcrypt/bcrypt.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "hash":
+		hash(os.Args[2])
+	case "compare":
+		compare(os.Args[2], os.Args[3])
+	default:
+		fmt.Printf("Invalid command: %v\n", os.Args[1])
+	}
+}
+
+func hash(password string) {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Printf("error hashing: %v\n", password)
+		return
+	}
+	fmt.Println(string(hashedBytes))
+}
+
+func compare(password, hash string) {
+	fmt.Printf("TODO: Compare password %q with hash %q\n", password, hash)
+}
